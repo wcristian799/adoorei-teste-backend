7cab68b9918cce9b032fb0f13fb14b23b75d87ae
Wellington Cristian <104509331+wcristian799@users.noreply.github.com>
2024-03-03 17:57:27 -0300

    Corrigido arquivo de migração da tabela products
    Adicionadas colunas 'name', 'price', e 'description' ao arquivo de migração da tabela products para refletir corretamente a estrutura da tabela.

--- database/migrations/2024_03_03_200105_create_products_table.php
+++ database/migrations/2024_03_03_200105_create_products_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price', 8, 2);
+            $table->text('description')->nullable();
             $table->timestamps();
         });
+        
     }
 
     /**
